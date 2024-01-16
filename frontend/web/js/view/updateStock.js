b90842a9d3da757358da9e13a19680bbe18ab1a5
Vinicius Araujo <90002191+viniciusaraujo05@users.noreply.github.com>
2024-01-16 21:43:07 +0000

    Style fix

---
+++ frontend/web/js/view/updateStock.js
@@ -0,0 +1,55 @@
+define([
+    'ko',
+    'uiComponent',
+    'mage/url',
+    'mage/storage',
+    'jquery'
+], function (ko, Component, urlBuilder, storage, $) {
+    'use strict';
+
+    return Component.extend({
+        defaults: {
+            template: 'Goit_ShowProductHome/product',
+        },
+
+        product: ko.observable({
+            name: '',
+            price: '',
+            stockQty: '',
+            src: '',
+            url: ''
+        }),
+
+        initialize: function () {
+            this._super();
+            this.getProduct();
+        },
+
+        getProduct: function () {
+            var self = this;
+            var serviceUrl = urlBuilder.build('showproduct/showproduct/index');
+            return storage.post(
+                serviceUrl,
+            ).done(
+                function (response) {
+                    var productData = JSON.parse(response);
+                    self.product({
+                        name: productData.name,
+                        price: productData.price,
+                        stockQty: productData.stockQty,
+                        src: productData.src,
+                        url: productData.url
+                    });
+
+                    setTimeout(function () {
+                        self.getProduct();
+                    }, productData.timeUpdate);
+                }
+            ).fail(
+                function (response) {
+                    alert(response);
+                }
+            );
+        },
+    });
+});
